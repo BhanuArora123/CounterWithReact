efe9e1926f9281ead20cbf60555a2f19a26f1cf5
Bhanu Arora <mrbhanuarora@gmail.com>
2021-11-28 12:19:18 +0530

    counter with redux completed

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,18 +1,44 @@
+import { Fragment, useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/counter";
 import classes from "./Auth.module.css";
 
 const Auth = props => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const submitHandler = event => {
+        event.preventDefault();
+        if (email.includes("@") && password) {
+            dispatch(authActions.login());
+        }
+    }
+    const emailHandler = event => {
+        setEmail(event.target.value);
+    }
+    const passHandler = event => {
+        setPassword(event.target.value);
+    }
     return (
-        <form className={classes["authForm"]}>
-            <div className={classes["field-wrap"]}>
-                <label className={classes["field-label"]}>Email</label>
-                <input type="email" className={classes["field-input"]} />
-            </div>
-            <div className={classes["field-wrap"]}>
-                <label className={classes["field-label"]}>PassWord</label>
-                <input type="password" className={classes["field-input"]}/>
-            </div>
-            <button>Login</button>
-        </form>
+        <Fragment>
+            {
+                !isAuthenticated &&
+                <form className={ classes["authForm"] } onSubmit={submitHandler}>
+                    <div className={ classes["field-wrap"] }>
+                        <label className={ classes["field-label"] }>Email</label>
+                        <input type="email" className={ classes["field-input"] }
+                        onChange={emailHandler} />
+                    </div>
+                    <div className={ classes["field-wrap"] }>
+                        <label className={ classes["field-label"] }>PassWord</label>
+                        <input type="password" className={ classes["field-input"] } onChange={passHandler}/>
+                    </div>
+                    <button>Login</button>
+                </form>
+            }
+        </Fragment>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import classes from "./Header.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/counter";
 const Header = props => {
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const dispatch = useDispatch();
+    const logoutHandler = () => {
+        dispatch(authActions.logout());
+    }
     return (
         <nav className={classes["navbar"]}>
             <h1 className={classes["navHead"]}>Redux Auth</h1>
@@ -11,7 +16,7 @@ const Header = props => {
                     <>
                         <div className={classes["menuItems"]}>My Products</div>
                         <div className={classes["menuItems"]}>My Sales</div>
-                        <button className={classes["logout"]}>Logout</button>
+                        <button className={classes["logout"]} onClick={logoutHandler}>Logout</button>
                     </>
                 }
             </div>
